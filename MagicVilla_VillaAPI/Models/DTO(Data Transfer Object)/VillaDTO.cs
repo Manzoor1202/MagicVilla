dca520d27b374e89f290cb9a3810fee945ed68f7
MA20276557 <MA20276557@DESKTOP-6060KAJ>
2024-05-23 14:34:42 +0530

    Add project files.

---
+++ MagicVilla_VillaAPI/Models/DTO(Data Transfer Object)/VillaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.DTO_Data_Transfer_Object_
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public int SqFt { get; set; }
+
+    }
+}
